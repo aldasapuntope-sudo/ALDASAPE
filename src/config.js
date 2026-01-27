350fb7d3f9b97aca0743293bb70bb587ad43656a
aldasapuntope-sudo <aldasapuntope@gmail.com>
2026-01-27 16:59:16 -0500

    2do cambio enero

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiUrl: "https://apidalsa.mycvsac.org.pe/",
-  urlserver: "https://apidalsa.mycvsac.org.pe/",
-  urlimageneslocal: "https://aldasa.pe/",
-  //apiUrl: "http://127.0.0.1:8000/",
-  //urlserver: "http://localhost/",
-  //urlimageneslocal: "http://localhost:3000",
+  //apiUrl: "https://apidalsa.mycvsac.org.pe/",
+  //urlserver: "https://apidalsa.mycvsac.org.pe/",
+  //urlimageneslocal: "https://aldasa.pe/",
+  apiUrl: "http://127.0.0.1:8000/",
+  urlserver: "http://localhost/",
+  urlimageneslocal: "http://localhost:3000",
 
   logotipo: 'image/logo/logo-unj-v1.svg',
   icon: 'image/favicon/unj.ico', 
